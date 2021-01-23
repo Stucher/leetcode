b0b5fab74d88703c2e6ba2017a720f2196d73bf8
lGuan <guanchangsheng@126.com>
2021-01-23 19:58:25 +0800

    [upload][104]树的深度[使用递归，叶子节点返回1，非叶子节点对左右子节点递归取较大值+1]

---
+++ main/leetcode_0104.go
@@ -0,0 +1,15 @@
+package main
+
+import "math"
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+
+	return int(math.Max(float64(maxDepth(root.Left)), float64(maxDepth(root.Right)))) + 1
+}
